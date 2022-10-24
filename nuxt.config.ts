52b2d8ee94aaa6864a288197a62d758ba7611826
koba1108 <paseli.koba@gmail.com>
2022-10-24 18:07:56 +0900

    fix workflow

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,3 @@
-import {firebaseConfig} from "./config";
 import {defineNuxtConfig} from 'nuxt/config'
 
 export default defineNuxtConfig({
@@ -8,14 +7,23 @@ export default defineNuxtConfig({
     head: {
       title: 'ULID Generator',
       meta: [
-        { name: 'description', content: 'Convert between ULID and datetime.' }
+        {name: 'description', content: 'Convert between ULID and datetime.'}
       ],
     }
   },
   buildModules: ["@nuxtjs/tailwindcss"],
   runtimeConfig: {
     public: {
-      firebaseConfig: {...firebaseConfig}
+      firebaseConfig: {
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        databaseURL: process.env.FIREBASE_DATABASE_URL,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.FIREBASE_APP_ID,
+        measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+      }
     },
   },
   vite: {
